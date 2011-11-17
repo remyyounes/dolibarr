30f397b2220d11341292b56aed2eb27c508eff5f
remyyounes <ryounes@gmail.com>
2011-11-17 19:50:00 -0400

    SupplierInvoiceNumbering Language

--- htdocs/admin/supplierinvoicenumbering.php
+++ htdocs/admin/supplierinvoicenumbering.php
@@ -34,6 +34,7 @@ require_once(DOL_DOCUMENT_ROOT.'/fourn/class/fournisseur.class.php');
 require_once(DOL_DOCUMENT_ROOT.'/fourn/class/fournisseur.facture.class.php');
 
 $langs->load("admin");
+$langs->load("supplierinvoicenumbering@supplierinvoicenumbering");
 
 if (!$user->admin)
 accessforbidden();
